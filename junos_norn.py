06ee11a2a1653625707e1b91cc65c162345a5187
juliopdx <juliop503@gmail.com>
2021-12-03 16:07:28 -0800

    Completed Jinja templates, vars, and deployment

--- junos_norn.py
+++ junos_norn.py
@@ -1,13 +1,25 @@
+"""Scripe used to interfact with some Junos gear"""
 from nornir import InitNornir
 from nornir_napalm.plugins.tasks import napalm_configure
 from nornir_utils.plugins.functions import print_result
+from nornir_utils.plugins.tasks.data import load_yaml
 from nornir_jinja2.plugins.tasks import template_file
+
+
+def load_network(task):
+    """Loads data to be added to each host from vars files"""
+    my_vars = task.run(task=load_yaml, file=f"./host_vars/{task.host}.yaml")
+    task.host["info"] = my_vars.result
+    deploy_network(task)
 
 
 def deploy_network(task):
     """Configures Junos device with NAPALM"""
     temp = task.run(
-        name="Building device configuration", task=template_file, path="templates", template="base.j2"
+        name="Building device configuration",
+        task=template_file,
+        path="templates",
+        template="base.j2",
     )
     configuration = temp.result
     task.run(
@@ -21,7 +33,7 @@ def deploy_network(task):
 def main():
     """Used to run all the things"""
     norn = InitNornir(config_file="configs/config.yaml", core={"raise_on_error": True})
-    result = norn.run(task=deploy_network)
+    result = norn.run(task=load_network)
     print_result(result)
 
 
