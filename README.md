51cadee031aa409e3dfd14b2bc2c697a837a8515
juliopdx <juliop503@gmail.com>
2021-12-06 15:40:27 -0800

    Add req and cleaned up host naming

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# junos-auto
-Learning Juniper Junos and some automation
+# Automating Junos Device Configuration with NAPALM and Nornir
+
+This is an example repository showing some concpets to automate Junos devices with NAPALM and Nornir. The configuration files are created from Jinja templates that will then be pushed to devices using NAPALM.
+
+[Blog post about Junos and automation](https://juliopdx.com/2021/12/06/my-journey-learning-about-juniper-junos/)
--- junos_norn.py
+++ junos_norn.py
@@ -1,4 +1,4 @@
-"""Scripe used to interfact with some Junos gear"""
+"""Script used to interfact with some Junos gear"""
 from nornir import InitNornir
 from nornir_napalm.plugins.tasks import napalm_configure
 from nornir_utils.plugins.functions import print_result
@@ -6,26 +6,31 @@ from nornir_utils.plugins.tasks.data import load_yaml
 from nornir_jinja2.plugins.tasks import template_file
 
 
-def load_network(task):
+def load_vars(task):
     """Loads data to be added to each host from vars files"""
     my_vars = task.run(task=load_yaml, file=f"./host_vars/{task.host}.yaml")
     task.host["info"] = my_vars.result
-    deploy_network(task)
+    template_configurations(task)
 
 
-def deploy_network(task):
-    """Configures Junos device with NAPALM"""
+def template_configurations(task):
+    """Builds network templates"""
     temp = task.run(
         name="Building device configuration",
         task=template_file,
         path="templates",
         template="base.j2",
     )
-    configuration = temp.result
+    config = temp.result
+    deploy_configurations(task, config)
+
+
+def deploy_configurations(task, config):
+    """Loads device configurations"""
     task.run(
-        name=f"Configuring {task.host.name}!",
+        name=f"Configuring {task.host}!",
         task=napalm_configure,
-        configuration=configuration,
+        configuration=config,
         replace=True,
     )
 
@@ -33,7 +38,7 @@ def deploy_network(task):
 def main():
     """Used to run all the things"""
     norn = InitNornir(config_file="configs/config.yaml", core={"raise_on_error": True})
-    result = norn.run(task=load_network)
+    result = norn.run(task=load_vars)
     print_result(result)
 
 
